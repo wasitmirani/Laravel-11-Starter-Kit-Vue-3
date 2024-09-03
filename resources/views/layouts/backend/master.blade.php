e61e8a8fb0c2c083a95559b1a6bb4d3c68a1a355
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2024-09-04 00:42:08 +0500

    update reset page

--- resources/views/layouts/backend/master.blade.php
+++ resources/views/layouts/backend/master.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }} || @yield('title') </title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <meta name="description" content="{{ config('app.name') }}">
     <meta name="keywords" content="{{ config('app.name') }}">
     <!-- Canonical SEO -->
