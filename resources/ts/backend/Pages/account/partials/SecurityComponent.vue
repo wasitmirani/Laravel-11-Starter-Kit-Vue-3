cea11d4290e6d61927d3b95d20df9498c33988e2
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2024-09-21 19:38:47 +0500

    update new changes on security component

--- resources/ts/backend/Pages/account/partials/SecurityComponent.vue
+++ resources/ts/backend/Pages/account/partials/SecurityComponent.vue
@@ -169,6 +169,60 @@
     </div>
 
     <!--End API Key List & Access -->
+<!-- Recent Devices -->
+    <div class="card">
+      <h6 class="card-header">Recent Devices</h6>
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th class="text-truncate">Browser</th>
+              <th class="text-truncate">Device</th>
+              <th class="text-truncate">Location</th>
+              <th class="text-truncate">Recent Activities</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="text-truncate text-heading"><i class="ri-macbook-line ri-20px text-warning me-3"></i>Chrome on Windows</td>
+              <td class="text-truncate">HP Spectre 360</td>
+              <td class="text-truncate">Switzerland</td>
+              <td class="text-truncate">10, July 2021 20:07</td>
+            </tr>
+            <tr>
+              <td class="text-truncate text-heading"><i class="ri-android-line ri-20px text-success me-3"></i>Chrome on iPhone</td>
+              <td class="text-truncate">iPhone 12x</td>
+              <td class="text-truncate">Australia</td>
+              <td class="text-truncate">13, July 2021 10:10</td>
+            </tr>
+            <tr>
+              <td class="text-truncate text-heading"><i class="ri-smartphone-line ri-20px text-danger me-3"></i>Chrome on Android</td>
+              <td class="text-truncate">Oneplus 9 Pro</td>
+              <td class="text-truncate">Dubai</td>
+              <td class="text-truncate">14, July 2021 15:15</td>
+            </tr>
+            <tr>
+              <td class="text-truncate text-heading"><i class="ri-mac-line ri-20px text-info me-3"></i>Chrome on MacOS</td>
+              <td class="text-truncate">Apple iMac</td>
+              <td class="text-truncate">India</td>
+              <td class="text-truncate">16, July 2021 16:17</td>
+            </tr>
+            <tr>
+              <td class="text-truncate text-heading"><i class="ri-macbook-line ri-20px text-warning me-3"></i>Chrome on Windows</td>
+              <td class="text-truncate">HP Spectre 360</td>
+              <td class="text-truncate">Switzerland</td>
+              <td class="text-truncate">20, July 2021 21:01</td>
+            </tr>
+            <tr class="border-transparent">
+              <td class="text-truncate text-heading"><i class="ri-android-line ri-20px text-success me-3"></i>Chrome on Android</td>
+              <td class="text-truncate">Oneplus 9 Pro</td>
+              <td class="text-truncate">Dubai</td>
+              <td class="text-truncate">21, July 2021 12:22</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     </div>
 </template>
 
