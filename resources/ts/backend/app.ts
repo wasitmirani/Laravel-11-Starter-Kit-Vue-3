c171708963bb60960f8559476956e17fcf7462d2
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2024-11-03 17:24:22 +0500

    update user form

--- resources/ts/backend/app.ts
+++ resources/ts/backend/app.ts
@@ -14,31 +14,18 @@ import BreadcrumbComponent from "./Components/BreadcrumbComponent.vue";
 
 // const BreadcrumbComponent = () => import(`${component_path}/BreadcrumbComponent.vue`);
 import SearchInput from "./Components/SearchInputComponent.vue";
-// import LoadingBox from "./vue/backend/components/LoadingBoxComponent.vue";
-// import Avatar from "./vue/backend/components/AvatarComponent.vue";
-// import ValidateInput from "./vue/backend/components/ValidateInputComponent.vue";
-// import GenericInput from "./vue/backend/components/GenericInputComponent.vue";
-
-// import PrimeVue from 'primevue/config';
-// import Aura from '@primevue/themes/aura';
-// import ToastService from 'primevue/toastservice';
+import LoadingBox from "./Components/LoadingBoxComponent.vue";
+import Avatar from "./Components/AvatarComponent.vue";
+import ValidateInput from "./Components/ValidateInputComponent.vue";
+// import GenericInput from "./Components/GenericInputComponent.vue";
 import Uploader from 'vue-media-upload';
 import { useGlobal } from './global-composables'; // Import the composable
 import VueMultiselect from 'vue-multiselect'
 import "vue-multiselect/dist/vue-multiselect.css"
-
-
-
 import router from "./router";
-
-
-
-
 
 const pinia = createPinia()
 const app = createApp(App);
-
-
   let permissions = JSON.parse(`${localStorage.getItem('permissions')}`);
   app.directive("can", {
     beforeMount(el, binding, vnode) {
@@ -52,25 +39,17 @@ const app = createApp(App);
   });
 app.use(pinia);
 app.use(router);
-app.provide('useGlobal', useGlobal); // Provide the composable
-
-
-
-
+// app.provide('useGlobal', useGlobal); // Provide the composable
 app.component('BreadcrumbComponent',BreadcrumbComponent);
-// app.component('Uploader', Uploader);
-
+app.component('Uploader', Uploader);
 app.component('SearchInput',SearchInput);
-// app.component('LoadingBox',LoadingBox);
-// app.component('validate-input',ValidateInput);
+app.component('LoadingBox',LoadingBox);
+app.component('validate-input',ValidateInput);
 // app.component('generic-input',GenericInput);
 app.component('VueMultiselect',VueMultiselect);
-// app.component('Avatar',Avatar);
-//
-
-
+app.component('Avatar',Avatar);
 app.config.globalProperties.$router = router;
-app.use(VueTelInput); // Define default global options here (optional)
+app.use(VueTelInput);
 
 app.mount('#app');
 
